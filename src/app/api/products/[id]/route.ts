68216c25fa7b82fc3bdb97b0c4c621aaa89191df
viruz <nahuelsinka@gmail.com>
2023-10-10 16:40:40 -0300

    fixed type bug

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,16 +1,15 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { NextResponse } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
 
 const { NEXT_PUBLIC_SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY } = process.env
 
-interface NextApiRequestWithParams extends NextApiResponse {
+interface NextResponseWithParams extends NextResponse {
   params: {
     id: string
   }
 }
 
-export async function GET (req: NextApiRequest, res: NextApiRequestWithParams) {
+export async function GET (req: NextRequest, res: NextResponseWithParams) {
   const { id } = res.params
 
   const supabase = createClient(NEXT_PUBLIC_SUPABASE_URL ?? '', SUPABASE_SERVICE_ROLE_KEY ?? '')
